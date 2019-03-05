083c82085f99d578d87f52cf713759833e42bb12
Aleksandr <duxard@gmail.com>
2019-03-05 01:09:41 -0500

    engine templates

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ const cl = console.log;
 const express = require('express'),
       app = express();
 
-//https://www.youtube.com/watch?v=_zvWeGwVkCY
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.get('/api/:id', (req, res) => {
+    res.send(`id is: ${req.params.id}`);
+});
+
+app.get('/profile/:name', (req, res) => {
+    const data = {age: 30, location: "unknown"}
+    res.render("profile", { person: req.params.name, data: data });
+});
+
+
+app.listen(3000);
+//https://www.youtube.com/watch?v=RczQp3zCPXs&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=26
